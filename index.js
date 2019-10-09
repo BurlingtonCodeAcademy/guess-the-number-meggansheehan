8d0c759d08ce320fec66446e6ee5c900345b609d
Habenzy <robertrstauss@gmail.com>
2019-10-09 11:48:26 -0400

    branch for feedback pr

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function ask(questionText) {
   });
 }
 
+start();
 
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
@@ -14,37 +15,4 @@ async function start() {
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
   process.exit();
-}
-
-
-const lowerLimit = 1;
-const upperLimit = 100;
-const tries = 20;
-const answer = math.floor(math.random () * (upperLimit - lowerLimit + 1)) + lowerLimit;
-const guess = '';
-const message = 'Guess a number between ' + lowerLimit + 'and' + upperLimit + ':';
-
-    while (isFinite(guess) && guess != '') {
-        guess = +guess;
-        if (guess < lowerLimit) {
-            alert('Your guess should not be lower than' + lowerLimit + '.');
-        }
-    else if (guess > upperLimit) {
-        alert('Your guess should not be greater than' + upperLimit + '.');
-    }
-    else if (guess < answer) {
-        alert('Too LOW');
-    }
-    else if (guess > answer) {
-        alert('Too HIGH');
-    }
-    else {
-        alert ('Winner Winner Chicken Dinner');
-        break;
-    }
-}  
-tries = tries - 1; 
-
-if (tries === 0) {
-    alert('You have no more tries');
 }
